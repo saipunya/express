cbcf60f36862044ca2951b7a1de1aec6844bb873
saipunya <sumetchoorat@gmail.com>
2024-06-24 16:08:23 +0700

    update24062567

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const engine = require('ejs')
 const path = require('path')
 app.set('view engine', 'ejs')
-
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use('/dist', express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.get('/', (req, res) =>{
-    res.render('index',{
-        title : 'Welcome to website',
-    })
+    res.redirect('product')
 })
 
-app.get('/person', (req, res) => {
+app.all('/search' , (req, res) =>{
+
+    // let query = req.query
+    let data = {}
+    if(Object.keys(req.query).length > 0){
+        data = {
+            q : req.query.q,
+            p : req.query.p,
+
+        }
+    }
+    res.render('search', data)
+})
+
+app.all('/person', (req, res) => {
     res.render('person',{
         firstname: 'John',
         lastname: 'Smith',
@@ -19,7 +33,7 @@ app.get('/person', (req, res) => {
     })
 })
 
-app.get('/product', (req, res) => {
+app.all('/product2', (req, res) => {
     res.render('product',{
         title : 'product',
         products : {
@@ -27,6 +41,40 @@ app.get('/product', (req, res) => {
             p2 : {name : 'computer', price : 22500 , color:'red'},
         }
     })
+})
+
+app.get('/test', (req, res) => {
+    res.render('add')
+})
+
+app.all('/add', (req, res) => {
+    let data = {}
+    if(req.body.num1){
+        let num1 = req.body.num1
+        let num2 = req.body.num2
+        let r = parseFloat(num1) + parseFloat(num2)
+        data = {
+            n1 : num1,
+            n2 : num2,
+            r : r,
+        }
+    }
+    res.render('add', data)
+})
+
+app.all('/product',(req, res) => {
+    let data = {}
+    if(req.body.product){
+        data = {
+            product : req.body.product,
+            price : req.body.price,
+            quantity : req.body.quantity,
+            desc : req.body.desc,
+            date_add : req.body.date_add.split('-').reverse().join('-'),
+        }
+
+    }
+    res.render('product',data)
 })
 
 
