46c7bd2bbf55fdb41ee2a4a6ab796475799e7379
saipunya <sumetchoorat@gmail.com>
2024-06-24 10:33:38 +0700

    update24062567

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const engine = require('ejs')
 const path = require('path')
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) =>{
+    res.render('index',{
+        title : 'Welcome to website',
+    })
+})
+
 app.get('/person', (req, res) => {
     res.render('person',{
         firstname: 'John',
         lastname: 'Smith',
         address : 'Bankok',
         link : '<a href="http://www.google.co.th">link</a>',
+    })
+})
+
+app.get('/product', (req, res) => {
+    res.render('product',{
+        title : 'product',
+        products : {
+            p1 : {name : 'table', price : 500 , color:'green'},
+            p2 : {name : 'computer', price : 22500 , color:'red'},
+        }
     })
 })
 
